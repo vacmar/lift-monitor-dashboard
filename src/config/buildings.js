430273e8b18a4f4efb96c5e06a0d37b4c0bd8cd5
Tarakesh-sampath <tarakeshsampath@gmail.com>
2025-08-04 00:52:28 +0530

    QOC2

--- src/config/buildings.js
+++ src/config/buildings.js
@@ -4,3 +4,26 @@ export const buildings = [
   "Belly 3",
   "Belly 4"
 ];
+
+
+/* expected api response structure
+{
+  "Belly 1": [
+    { ID: 'P1', Fl: '13', Alarm: '1', Door: '0' },
+    { ID: 'P2', Fl: '5', Alarm: '0', Door: '1' },
+    { ID: 'P3', Fl: '9', Alarm: '0', Door: '0' }
+  ],
+  "Belly 2": [
+    { ID: 'P4', Fl: '1', Alarm: '1', Door: '1' },
+    { ID: 'P5', Fl: '6', Alarm: '0', Door: '0' }
+  ],
+  "Belly 3": [
+    { ID: 'P6', Fl: '12', Alarm: '0', Door: '1' }
+  ],
+  "Belly 4": [
+    { ID: 'P1', Fl: '8', Alarm: '0', Door: '0' },
+    { ID: 'P2', Fl: '15', Alarm: '1', Door: '1' },
+    { ID: 'P3', Fl: '3', Alarm: '0', Door: '0' }
+  ]
+}
+*/
